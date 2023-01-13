449c43fe6a034cce0a6fb7a1d67ce964c35d6d3d
Oanh Nguyen <myspamvn@yahoo.com>
2023-01-13 23:53:44 +0900

    build: install devise-jwt for User model

--- House-Review-API/config/initializers/cors.rb
+++ House-Review-API/config/initializers/cors.rb
@@ -17,11 +17,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins '*'
-    origins 'http://localhost:5173/'
+    origins '*'
+    # origins 'http://localhost:5173/'
 
     resource '*',
              headers: :any,
-             methods: %i[get post put patch delete options head]
+             methods: %i[get post put patch delete options head],
+             expose: ['Authorization']
   end
 end
--- House-Review-API/config/routes.rb
+++ House-Review-API/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :reviews
   resources :buildings
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
