19d19a1b5f0565d7787ab712e5e34022fec90741
Oanh Nguyen <ONguyen@HTMJPN.local>
2023-01-18 18:14:25 +0900

    feat(forms): add search-view and search-bar

---
+++ House-Review-Client/src/components/Search.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+import SearchBar from './SearchBar.vue';
+
+//test
+const msg = 'Hello'
+function log() {
+console.log(msg)
+}
+
+const count = ref(0)
+const url = ref('')
+
+</script>
+
+<template>
+    <SearchBar searchObject="url"/>
+    <SearchBar searchObject="price"/>
+    <SearchBar searchObject="area"/>
+    <SearchBar searchObject="layout"/>
+    <h1>Lalala</h1>
+    <button @click="log">{{ msg }}</button>
+    <button @click="count++">{{ count }}</button>
+</template>
