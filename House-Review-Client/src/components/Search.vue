b8db2ee10dc7e36995544cb2132a9f32bd770674
Oanh Nguyen <ONguyen@HTMJPN.local>
2023-01-23 16:44:16 +0900

    feat(form): create search menu layout

--- House-Review-Client/src/components/Search.vue
+++ House-Review-Client/src/components/Search.vue
@@ -1,23 +1,47 @@
 <template>
-    <div id="search-apartment">        
-        <div class="font-bold border-solid border border-purple-200">
-            <input type="text" 
-                placeholder="apartment url" 
-                id="search-url"
-                v-model="apartmentUrl"
-            >
+    <div id="search-apartment" class="m-4 border-solid border border-purple-200">        
+        <div class="border-solid border border-purple-200">
+            <div class="grid grid-cols-12">
+                <div class="col-span-11">
+                    <input type="text" class="w-full"
+                    placeholder="apartment url" 
+                    id="search-url"
+                    v-model="apartmentUrl"
+                    >
+                </div>
+                <button class="border-solid border border-red-200">
+                    P
+                </button>
+            </div>
         </div>
-        <div class="font-bold">
-            <input type="text" placeholder="price">
+        <div class="my-2 border-solid border border-red-200">
+            <div class="grid grid-cols-12 gap-1">
+                <div class="col-span-5">
+                    <div class="flex pr-1 border-solid border border-green-200">
+                        <input type="text" placeholder="min" class="w-1/2 px-0.5">
+                        <div>~</div>
+                        <input type="text" placeholder="max" class="w-1/2 px-0.5">
+                        <div>¥</div>
+                    </div>
+                </div>
+                <div class="col-span-4">
+                    <div class="border-solid border border-pink-200">
+                        <input type="text" placeholder="area" class="w-full px-1">
+                    </div>
+                </div>
+                <div class="col-span-2">
+                    <div class="border-solid border border-green-200">
+                        <input type="text" placeholder="layout" class="w-full px-1">
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="border-solid border border-green-200">
+                        >
+                    </div>
+                </div>        
+            </div>
+            <button class="container mx-auto my-1.5 border-solid border border-green-200" @click="search()">Search</button>        
         </div>
-        <div class="font-bold">
-            <input type="text" placeholder="area">
-        </div>
-        <div class="font-bold">
-            <input type="text" placeholder="layout">
-        </div>        
-        <button @click="search()">Search</button>
-        <button @click="count++">{{ count }}</button>
     </div>
 </template>
 
@@ -25,10 +49,10 @@
 import { ref, onMounted } from 'vue';
 
 const apartmentUrl = ref('')
-const price = ref(0)
+const minPrice = ref(0)
+const maxPrice = ref(0)
 const area = ref(0)
 const layout = ref('')
-const count = ref(0) // test, can delete
 
 function search() {    
     const searchHouse = document.getElementById('search-url');
