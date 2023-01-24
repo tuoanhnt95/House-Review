45bdb0d308ed53fd51bee60687c636061457ead5
Oanh Nguyen <ONguyen@HTMJPN.local>
2023-01-24 14:54:23 +0900

    feat(form): add advanced search filter holders Size, station, floor, foreign friendly, musical instrument, pet, diy

--- House-Review-Client/src/main.js
+++ House-Review-Client/src/main.js
@@ -11,10 +11,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faMinus } from '@fortawesome/free-solid-svg-icons'
 /* add icons to the library */
 library.add(faMagnifyingGlass)
-library.add(faCaretRight)
+library.add(faPlus)
+library.add(faMinus)
 
 const pinia = createPinia()
 const app = createApp(App)
